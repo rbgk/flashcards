e0fb2a83dec8f695a3246133ec29d6f65dcf4917
Aaron <50600773+rbgk@users.noreply.github.com>
2023-10-17 18:59:15 -0400

    text box aesthetics

---
+++ js/aesthetics.js
@@ -0,0 +1,7 @@
+/**
+ * define inputBox from html
+ * assign placeholder value 
+ * using JS to keep html structure orderly
+*/
+const inputBox = document.getElementById("inputBox")
+inputBox.placeholder = `Word 1,Definition 1 \nWord 2,Definition 2 \nWord 3,Definition 3`;
