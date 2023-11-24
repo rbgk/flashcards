44907f90b212908821580c57b7cb164e63b58943
Aaron <50600773+rbgk@users.noreply.github.com>
2023-11-23 20:08:34 -0500

    take advantage of function defined in constants

--- js/toolbar.js
+++ js/toolbar.js
@@ -5,15 +5,13 @@ function saveToLocal() {
     // do not save empty input
     if (inputBox.value !== "") {
         localStorage.setItem("savedSet", inputBox.value);
-        gameStatusBar.style.color = "white";
-        gameStatusBar.innerHTML = "Set saved. Load it anytime";
+        defaultText("Set saved. Load it anytime");
     } else {
-        gameStatusBar.style.color = "red";
         // check if there is a saved set
         if (localStorage.getItem("savedSet") !== null) {
-            gameStatusBar.innerHTML = "Input is empty <br> Create a set to save or load saved set";
+            dangerText("Input is empty <br> Create a set to save or load saved set");
         } else {
-            gameStatusBar.innerHTML = "Input is empty <br> Create a set to save";
+            dangerText("Input is empty <br> Create a set to save");
         }
     }
 }
@@ -24,12 +22,10 @@ function loadSavedSet() {
     // value is exactly as user inputted
     let savedSet = localStorage.getItem("savedSet");
     if (savedSet === null || savedSet === "") {
-        gameStatusBar.style.color = "red";
-        gameStatusBar.innerHTML = "No saved set <br> Create or upload";
+        dangerText("No saved set <br> Create or upload");
     } else {
         inputBox.value = savedSet;
-        gameStatusBar.style.color = "white";
-        gameStatusBar.innerHTML = "Saved set loaded";
+        defaultText("Saved set loaded");
     }
 }
 
@@ -37,6 +33,5 @@ function loadSavedSet() {
 function clearInput() {
     // clear the inputBox
     inputBox.value = "";
-    gameStatusBar.style.color = "white";
-    gameStatusBar.innerHTML = "Text box cleared";
+    defaultText("Text box cleared");
 }
