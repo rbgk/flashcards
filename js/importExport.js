ac23cba97ca9538dc15ba5b4b838a860b85c4906
Michelle Prior <map398@cornell.edu>
2023-12-05 00:26:23 -0500

    changed formatting

--- js/importExport.js
+++ js/importExport.js
@@ -1,10 +1,13 @@
+//File reader for imports  
+const reader = new FileReader();
+
 /**
  * Import a file of type .csv or .txt
  * Populates the input box if file is not empty and is of the correct type
  */
 function importFile() {
-    const fileInput = document.getElementById('fileInput')
-    const resultTextBox = document.getElementById('inputBox');
+    let fileInput = document.getElementById('fileInput')
+    let resultTextBox = document.getElementById('inputBox');
     importFlow(fileInput, resultTextBox)
 }
 
@@ -23,19 +26,14 @@ function triggerImportPopup() {
  */
 function importFlow(fileInput, resultTextBox) {
     if (fileInput.files.length === 1) {
-        const selectedFile = fileInput.files[0];
-        const reader = new FileReader();
+        let selectedFile = fileInput.files[0]
         reader.onload = function (e) {
-            const fileContent = e.target.result;
-            const fileType = selectedFile.name.split('.').pop().toLowerCase();
-            populateBox(fileType, fileContent, resultTextBox)
+            populateBox(selectedFile.name.split('.').pop().toLowerCase(), e.target.result, resultTextBox)
         };
         reader.readAsText(selectedFile);
     }
     else if (fileInput.files.length > 1) {
         alert('Please select only one file.');
-    } else {
-        return
     }
 }
     
@@ -50,4 +48,27 @@ function populateBox(fileType, fileContent, resultTextBox) {
         alert('Invalid file type. Please select a .csv or .txt file.');
     }
 }
+
+/**
+ * Export a file of type .txt
+ * Will check to make sure it is not empty 
+ */
+ function exportTxt() {
+    const fileInput = document.getElementById('fileInput')
+    const resultTextBox = document.getElementById('inputBox');
+    importFlow(fileInput, resultTextBox)
+}
+
+/**
+ * Export a file of type .csv
+ * Will check to make sure it is not empty 
+ */
+ function exportCsv() {
+    const fileInput = document.getElementById('fileInput')
+    const resultTextBox = document.getElementById('inputBox');
+    importFlow(fileInput, resultTextBox)
+}
+
+
+
 
